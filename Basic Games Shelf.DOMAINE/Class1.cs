3d465ed9074f39e4990d1c0edda1b97ae117ef7b
StyKon <khalilfrikha30@gmail.com>
2023-01-21 14:59:44 +0100

    Creating Class Games

--- Basic Games Shelf.DOMAINE/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿
-namespace Basic_Games_Shelf.DOMAINE
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ Basic Games Shelf.DOMAINE/Games.cs
@@ -0,0 +1,17 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Basic_Games_Shelf.DOMAINE
+{
+    public class Games
+    {
+        [Key]
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Game { get; set; }
+        public int PlayTime { get; set; }
+        public string Genre { get; set; }
+        public string[] Platforms { get; set; }
+
+    }
+}
